f7f7547823645e4b425ac859e56a20d01dd52b16
Wille Faler <wille.faler@gmail.com>
2022-07-25 23:03:20 +0200

    fix this

--- internal/observability_test.go
+++ internal/observability_test.go
@@ -21,4 +21,25 @@ func TestMkObservabilityConfigs(t *testing.T) {
 	assert.NoError(t, err)
 	consul := &MockConsul{}
 	mkObservabilityConfigs(consul, inv, folder, "root")
+
+	assert.Equal(t, 5, len(consul.RegisterIntentionCalls()))
+	assert.Equal(t, 5, len(consul.RegisterServiceCalls()))
+
+	assert.Equal(t, 23, readDir(folder))
+}
+
+func readDir(str string) int {
+	count := 0
+	d, e := os.ReadDir(str)
+	if e != nil {
+		panic(e)
+	}
+	for _, f := range d {
+		if f.IsDir() {
+			count = count + readDir(filepath.Join(str, f.Name()))
+		} else {
+			count++
+		}
+	}
+	return count
 }
